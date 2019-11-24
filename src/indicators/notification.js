e5b4170997720adf2b76b7b28e72ba84b87b3314
Payson Wallach <paysonwallach@icloud.com>
2019-11-23 23:21:43 -0600

    Standardize license formatting

--- src/indicators/notification.js
+++ src/indicators/notification.js
@@ -1,22 +1,22 @@
 /*
-    This file is part of Extend Panel Menu
+ * This file is part of Extend Panel Menu
+ *
+ * Extend Panel Menu is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Extend Panel Menu is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
 
-    Extend Panel Menu is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Extend Panel Menu is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Extend Panel Menu.  If not, see <http://www.gnu.org/licenses/>.
-
-    Copyright 2019 Payson Wallach
-    Copyright 2017-2018 Julio Galvan
-*/
+ * You should have received a copy of the GNU General Public License
+ * along with Extend Panel Menu.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2019 Payson Wallach
+ * Copyright 2017-2018 Julio Galvan
+ */
 
 const Gio = imports.gi.Gio;
 const Gtk = imports.gi.Gtk;
